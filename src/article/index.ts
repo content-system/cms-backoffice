27744e59c23b0d9ffe3e94a20ed30c8ceb5dc4d9
Duc Nguyen <minhduc140583@gmail.com>
2024-10-20 23:49:28 +0700

    init project

---
+++ src/article/index.ts
@@ -0,0 +1,30 @@
+import { Controller } from "express-ext"
+import { Log, Manager, Search } from "onecore"
+import { DB, Repository, SearchBuilder } from "query-core"
+import { Article, ArticleFilter, articleModel, ArticleRepository, ArticleService } from "./article"
+export * from "./article"
+
+export class SqlArticleRepository extends Repository<Article, string> implements ArticleRepository {
+  constructor(db: DB) {
+    super(db, "articles", articleModel)
+  }
+}
+export class ArticleManager extends Manager<Article, string, ArticleFilter> implements ArticleService {
+  constructor(search: Search<Article, ArticleFilter>, repository: ArticleRepository) {
+    super(search, repository)
+  }
+}
+export class ArticleController extends Controller<Article, string, ArticleFilter> {
+  constructor(log: Log, service: ArticleService) {
+    super(log, service)
+  }
+}
+
+export function useArticleService(db: DB): ArticleService {
+  const builder = new SearchBuilder<Article, ArticleFilter>(db.query, "articles", articleModel, db.driver)
+  const repository = new SqlArticleRepository(db)
+  return new ArticleManager(builder.search, repository)
+}
+export function useArticleController(log: Log, db: DB): ArticleController {
+  return new ArticleController(log, useArticleService(db))
+}
